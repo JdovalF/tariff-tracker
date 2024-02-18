811c4a5cb5d045441fee7a3e39684bd344a571c8
jadoval <jadoval@opentrends.net>
2024-02-18 09:59:23 +0100

    initial commit

---
+++ src/main/java/com/example/tarifftracker/TariffTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.tarifftracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TariffTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TariffTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tarifftracker/TariffTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tarifftracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TariffTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
